76d4e186badb8b71e28ad980c895b195efeb270e
samuel <s.mtz.casillas@gmail.com>
2017-09-21 15:46:04 -0700

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Valida cantidad de entrega en albaranes de salida',
+    'version': '1.1',
+    'summary': 'Al validar un albarán de salida, se valida que la cantidad por entregar no sea mayor a la establecida.',
+    'category': 'Denker',
+    'description': """
+    Al validar un albarán de salida, se valida que la cantidad por entregar no sea mayor a la establecida.
+    """,
+    'author': 'Humanytek',
+    'website': 'http://www.humanytek.com',
+    'depends': ['stock'],
+    'data': [
+        
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ stock_picking.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from openerp import addons
+from openerp import models, fields, api, _
+from openerp.exceptions import UserError, RedirectWarning, ValidationError
+
+
+class stock_picking(models.Model):
+	_inherit = ['stock.picking']
+
+
+	@api.multi
+	def do_new_transfer(self):
+		if self.picking_type_id.code == 'outgoing' and self.pack_operation_ids:
+			for operation in self.pack_operation_ids:
+				if operation.qty_done > operation.product_qty:
+					raise ValidationError(_('No se pueda entregar más de lo que está establecido.'))
+
+		return super(stock_picking,self).do_new_transfer()
