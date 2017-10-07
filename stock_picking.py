e2f9c969dbd61f351dae8de81b3b692d63b84274
edniemand <edniemand@users.noreply.github.com>
2017-10-06 21:00:01 -0700

    Add files via upload
    extiende la funcionalidad a los internal type

--- stock_picking.py
+++ stock_picking.py
@@ -11,7 +11,7 @@ class stock_picking(models.Model):
 
 	@api.multi
 	def do_new_transfer(self):
-		if self.picking_type_id.code == 'outgoing' and self.pack_operation_ids:
+		if (self.picking_type_id.code == 'outgoing' or self.picking_type_id.code == 'internal') and self.pack_operation_ids:
 			for operation in self.pack_operation_ids:
 				if operation.qty_done > operation.product_qty:
 					raise ValidationError(_('No se pueda entregar más de lo que está establecido.'))
